f27687e8a0549c622783544f4bdf6a56a70e928b
Dani-dzoe <danieldzotepe568@gmail.com>
2023-03-08 09:36:10 -0800

    task 0

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked lists
+ * @n: integer
+ * @next: points to the next code
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, cont=st int n);
+void free listint(listint_t *head);
+listint_t *insert_node(listint_t **head, int number);
+
+#endif
