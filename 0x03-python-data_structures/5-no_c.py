da941a1b5fb4d1452e48ea37c9cc77b8950572f9
Dani-dzoe <danieldzotepe568@gmail.com>
2023-03-13 22:25:10 -0700

    task 0 to 8

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-no_c.py
+
+def no_c(my_string):
+    """Remove all characters c and C from a string."""
+    copy = [x for x in_string if x != 'c'an x != 'C']
+    return ("".join(copy))
