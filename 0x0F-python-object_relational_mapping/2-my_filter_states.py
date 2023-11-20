fd0a7763588850c92e0e4241e6c892e86a84b75d
Dani-dzoe <danieldzotepe568@gmail.com>
2023-11-20 10:45:13 -0800

    some few fixes and most task completed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-
 """
-Script that takes in an argument and displays all values in the states 
-table of hbtn_0e_0_usa where name matches the argument.
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
-import sys from argv
+from sys import argv
 
 if __name__ == '__main__':
     """
-    Access the databases to list the states
+    Access to the database and get the states
+    from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("select * from states \
-            where name like binary '{}' \
-            order by states ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-""" script that takes in arguments and displays all values in the 
-states table of hbtn_0e_0_usa where name matches
-the argument. But this time, write one that is safe from MySQL injections!
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+
+This time the script is safe from
+MySQL injections!
 """
 
 import MySQLdb
-import sys from argv
+from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database to list the states
+    Access to the database and get the states
+    from the database.
     """
-    db = MySQldb.connect(host="localhost", user=argv[1], port=3306, 
-            passwd=argv[2], db=argv[3])
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     with db.cursor() as cur:
-        cur.execute("select * from states \
-                where name like binary $(name)s \
-                order by states.id" {
-                    'name': argv[4]
-                    })
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+
         rows = cur.fetchall()
 
     if rows is not None:
